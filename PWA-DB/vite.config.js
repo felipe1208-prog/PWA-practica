d1e670f23513fb84b14ab2d0b7701fe6e9a43b59
Felipe <sfortizrodriguez@gmail.com>
2026-08-15 13:38:26 -0400

    configuracion terminada para desarrollo

--- PWA-DB/vite.config.js
+++ PWA-DB/vite.config.js
@@ -4,45 +4,51 @@ import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: "prompt",
-      manifest: {
-        name: "DBPWA",
-        short_name: "Gestión Clientes DB",
-        description: "PWA para gestión de clientes de Disbattery",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        icons: [
-          {
-            src: "/image-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/image-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/image-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "prompt",
+            manifest: {
+                name: "DBPWA",
+                short_name: "Gestión Clientes DB",
+                description: "PWA para gestión de clientes de Disbattery",
+                theme_color: "#ffffff",
+                background_color: "#ffffff",
+                display: "standalone",
+                icons: [
+                    {
+                        src: "/image-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/image-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/image-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    },
+                ],
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
+    server: {
+        port: 3000,
+        host: true,
+        allowedHosts: true,
+    },
+    preview: {
+        port: 3000,
+        host: true,
+        allowedHosts: true,
+    },
 });
