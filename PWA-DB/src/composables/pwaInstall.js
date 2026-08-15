dd6f7c011ddeb6aa9ee37564be36d5d3d409a5bc
Felipe <sfortizrodriguez@gmail.com>
2026-08-15 11:51:56 -0400

    instalacion pwa

---
+++ PWA-DB/src/composables/pwaInstall.js
@@ -0,0 +1,37 @@
+import { ref, onMounted, onUnmounted } from "vue";
+
+export function pwaInstall() {
+    const eventoInstalacion = ref(null);
+    const esInstalable = ref(false);
+    const validarInstalacion = (evento) => {
+        evento.preventDefault();
+        eventoInstalacion.value = evento;
+        esInstalable.value = true;
+    };
+
+    const instalarPwa = async () => {
+        if (!eventoInstalacion.value) return;
+
+        eventoInstalacion.value.prompt(); //ventana de confirmacion de descarga
+
+        const { outcome } = await eventoInstalacion.value.userChoice;
+        console.log(`Usuario escogió: ${outcome}`);
+
+        eventoInstalacion.value = null;
+        esInstalable.value = true;
+    };
+
+    onMounted(() => {
+        window.addEventListener("beforeinstallprompt", validarInstalacion);
+    });
+
+    onUnmounted(() => {
+        //si quita para no consumir recursos al recargar
+        window.removeEventListener("beforeinstallprompt", validarInstalacion);
+    });
+
+    return {
+        esInstalable,
+        instalarPwa,
+    };
+}
