90f8a2d1e9afb7922178362b27fec68897cfdabd
Felipe <sfortizrodriguez@gmail.com>
2026-08-15 10:31:21 -0400

    confiugarion vite-pwa

--- PWA-DB/vite.config.js
+++ PWA-DB/vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "prompt",
+      includeAssets: ["hero.png"],
+      manifest: {
+        name: "DBPWA",
+        short_name: "Gestión Clientes DB",
+        description: "PWA para gestión de clientes de Disbattery",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/ico.ico",
+            sizes: "any",
+            type: "image/icos.ico",
+          },
+        ],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+});
