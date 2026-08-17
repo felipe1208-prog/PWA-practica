fd397231b0f2d71511c752b648cdc6c4d5dda03f
Felipe <sfortizrodriguez@gmail.com>
2026-08-17 09:26:13 -0400

    axios/api y pinia agregados

--- PWA-DB/src/main.js
+++ PWA-DB/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 import router from "./router/index.js";
 
 const app = createApp(App);
